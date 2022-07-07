2d4dfddbd234eec6a7710b454659b1ece52471df
onnamani <68425897+onnamani@users.noreply.github.com>
2022-07-07 13:07:21 +0100

    connecting node app to mongodb service

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+            .then(() => console.log("successfully connected to DB"))
+            .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h2>There you are!!</h2>");
