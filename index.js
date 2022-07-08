4a0904d81cb8de4d728eb0cbc612c68aeaee0f7d
onnamani <68425897+onnamani@users.noreply.github.com>
2022-07-08 23:14:25 +0100

    adding session user session login - persisting with redis

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require("express");
 const { connectToMongo } = require("./db/mongoDB");
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
+const { REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config");
+const session = require("express-session");
+const { createClient } = require("redis");
+let RedisStore = require("connect-redis")(session);
+
+let redisStore;
+
+let redisClient = createClient({
+    socket: {
+        host: REDIS_URL,
+        port: REDIS_PORT
+    }
+});
+
+redisClient.connect().then((result) => {
+    redisStore = new RedisStore({ client: redisClient })
+})
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+connectToMongo(app, port);
 
-// app.get("/", (req, res) => {
-//     res.send("<h2>There you are!!!!</h2>");
-// })
+
+app.use(session({
+    store: redisStore,
+    secret: SESSION_SECRET,
+    cookie: {
+        secure: false,
+        resave: false,
+        saveUninitialized: false,
+        httpOnly: true,
+        maxAge: 30000
+    }
+}))
 
 app.use(express.json());
 
@@ -17,6 +45,3 @@ app.use("/api/v1/posts", postRouter);
 
 //localhost:3000/api/v1/users
 app.use("/api/v1/users", userRouter);
-
-const port = process.env.PORT || 3000;
-connectToMongo(app, port);
