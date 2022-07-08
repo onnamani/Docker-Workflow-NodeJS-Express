5c5a3776fd8a91a13860fd74ecf3805b37ed96e7
onnamani <68425897+onnamani@users.noreply.github.com>
2022-07-08 15:25:15 +0100

    created user creation and authenticatioin rest service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectToMongo } = require("./db/mongoDB");
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -13,6 +14,9 @@ app.use(express.json());
 
 //localhost:3000/api/v1/posts
 app.use("/api/v1/posts", postRouter);
+
+//localhost:3000/api/v1/users
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000;
 connectToMongo(app, port);
