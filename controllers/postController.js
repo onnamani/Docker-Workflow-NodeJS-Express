aa9d8bcefb91682e6adf99ddb9ee242e7183c20d
onnamani <68425897+onnamani@users.noreply.github.com>
2022-07-08 11:16:16 +0100

    created a crud blog post api service

---
+++ controllers/postController.js
@@ -0,0 +1,86 @@
+const Post = require("../models/postModel");
+
+exports.getAllPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find();
+
+        res.status(200).json({
+            status: "success",
+            results: posts.length,
+            data: {
+                posts
+            }
+        });
+    } catch(e) {
+        res.status(404).json({
+            status: "posts not found"
+        })
+    }
+}
+
+exports.getOnePost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                post
+            }
+        });
+    } catch(e) {
+        res.status(404).json({
+            status: "post not found"
+        })
+    }
+}
+
+exports.createPost = async (req, res, next) => {
+    try {
+        const post = await Post.create(req.body)
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                post
+            }
+        });
+    } catch(e) {
+        res.status(400).json({
+            status: "fail to register"
+        })
+    }
+}
+
+exports.updatePost = async (req,res, next) => {
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, 
+                                                    req.body, 
+                                                    { new: true, runValidators: true });
+        
+        res.status(200).json({
+            status: "success",
+            data: {
+                post
+            }
+        });               
+    } catch(e) {
+        res.status(400).json({
+            status: "failed to update"
+        })
+    }
+}
+
+exports.deletePost = async (req, res, next) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            status: "success"
+        })
+    } catch(e) {
+        res.status(400).json({
+            status: "failed to delete post"
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { connectToMongo } = require("./db/mongoDB");
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
 
 
-app.get("/", (req, res) => {
-    res.send("<h2>There you are!!!!</h2>");
-})
+// app.get("/", (req, res) => {
+//     res.send("<h2>There you are!!!!</h2>");
+// })
+
+app.use(express.json());
+
+//localhost:3000/api/v1/posts
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
-
 connectToMongo(app, port);
